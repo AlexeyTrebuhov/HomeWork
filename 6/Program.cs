51b2ce5cc47aae61863a71b9665f33b150097ac2
Alex_T <saler@inbox.ru>
2022-02-02 23:35:45 +0300

    Определение четного числа

---
+++ 6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите любое целое число и нажмите клавишу Enter: ");
+string string1 = Console.ReadLine();
+int arg1 = Convert.ToInt32(string1);
+
+if ( arg1 % 2 == 0 )
+{
+Console.WriteLine (arg1);  
+Console.WriteLine (" Это число четное");}
+
+else
+{
+Console.WriteLine (arg1);  
+Console.WriteLine (" Это число четное. Не очень  (Борат)");}
