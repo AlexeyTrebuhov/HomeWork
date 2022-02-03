c834c75fb67805edde8e5f86d88eeda4e4a4d30f
Alex_T <saler@inbox.ru>
2022-02-03 19:11:42 +0300

    Вывод четных чисел от 1 до N

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,26 +1,26 @@
 ﻿Console.Write("Введите любое целое число и нажмите клавишу Enter: ");
 string string1 = Console.ReadLine();
 int arg1 = Convert.ToInt32(string1);
-
-    if ( arg1 % 2 == 0 )
-    {
+   
+        if ( arg1 % 2 == 0 )
+        {
         int index = arg1;
-        while ( index == 2 )
-        {   
+        while ( index > 0 )
+            {   
             Console.WriteLine (index); 
             index = index - 2; 
+            }
         }
-    }
 
-    else
-    {
-Console.WriteLine (arg1);  
-Console.WriteLine (" Это число четное. Не очень  (Борат)");
-int index = arg1 - 1;
-            
-    while ( index == 2 )
-        {   
-        Console.WriteLine (index); index = index - 2; 
+        else
+        {
+
+            int index = arg1 - 1;
+            while ( index > 0 )
+            {   
+            Console.WriteLine (index); 
+            index = index - 2; 
+            }
         }
-    }
+         
     
