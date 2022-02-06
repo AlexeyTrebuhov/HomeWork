4849ddfd0139e25b2c8d31f33cf6080814a633a1
Alex_T <saler@inbox.ru>
2022-02-06 23:17:39 +0300

    удалить вторую цифру трехзначного числа

---
+++ 12/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число и нажмите клавишу Enter: ");
+string string1 = Console.ReadLine();
+int arg1 = Convert.ToInt32(string1);
+
+
+Console.WriteLine ("Если удалить вторую цифру, получится число");
+Console.WriteLine(string1.Remove(1,1));
