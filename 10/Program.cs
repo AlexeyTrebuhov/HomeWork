bfec6cb06a6c73749e6455283efbd423d712fc66
Alex_T <saler@inbox.ru>
2022-02-06 22:40:50 +0300

    вторая цифра трехзначного числа

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,1 +1,9 @@
 ﻿// Показать вторую цифру трёхзначного числа.
+
+Console.Write("Введите трехзначное число и нажмите клавишу Enter: ");
+string string1 = Console.ReadLine();
+int arg1 = Convert.ToInt32(string1);
+
+
+Console.WriteLine ("Вторая цифра данного числа - это");
+Console.WriteLine(string1[1]);
