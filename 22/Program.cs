bbe7e0de68ba9877554b7de7913adf3eb15bec6c
Alex_T <saler@inbox.ru>
2022-02-09 00:12:22 +0300

    Задания 22

---
+++ 22/Program.cs
@@ -0,0 +1,33 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+Console.Write("Выберите пространство 2D (цифра 1 ) или 3D (цифра 2)  и  нажмите клавишу Enter: ");
+int N = int.Parse (Console.ReadLine());
+
+if ( N == 1)
+    {
+    Console.WriteLine(" Введите координату x для точки 1 и нажмите Enter");
+    int x1 = int.Parse (Console.ReadLine());
+    Console.WriteLine(" Введите координату Y для точки 1 и нажмите Enter");
+    int y1 = int.Parse (Console.ReadLine());
+    Console.WriteLine(" Введите координату x для точки 2 и нажмите Enter");
+    int x2 = int.Parse (Console.ReadLine());
+    Console.WriteLine(" Введите координату Y для точки 2 и нажмите Enter");
+    int y2 = int.Parse (Console.ReadLine());
+        
+        if (x1 >= 0)
+        {
+            int x = x1 + x2;
+        }
+        else 
+        {
+            int x1 = 
+        }
+
+    Console.WriteLine(x1+x2);
+    }
+
+else
+    {
+    Console.WriteLine("7");
+    }
+
