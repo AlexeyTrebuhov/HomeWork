794100eece03f67131c3b766dd5eab4db676bbe6
Alex_T <saler@inbox.ru>
2022-02-08 23:48:18 +0300

    Задания 24

---
+++ 24/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Введите число больше ноля и  нажмите клавишу Enter: ");
+int N = int.Parse (Console.ReadLine());
+
+for ( int i = 1; i <= N; i++ )
+Console.WriteLine ( i*i*i);
