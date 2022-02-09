0cb4ca0f5cbb5de9e30d1be9b10c3b80017cf5e9
Alex_T <saler@inbox.ru>
2022-02-10 01:37:28 +0300

    Задания 18

---
+++ 18/Program.cs
@@ -0,0 +1,28 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+Console.Write("Укажите значение операнда Х: 1 - если True, 2 - если False");
+int x = int.Parse (Console.ReadLine());
+Console.Write("Укажите значение операнда Y: 1 - если True, 2 - если False");
+int y = int.Parse (Console.ReadLine());
+
+bool c = x | y;
+
+    if ( x == 1, y ==1 )
+
+if (arg1 < 8)
+{
+if (arg1 == 1)
+    Console.WriteLine ("Это совсем не выходной. Это понедельник");
+if (arg1 == 2)
+    Console.WriteLine ("Это не похоже на выходной. Это вторник");
+else
+Console.WriteLine ("Этот день недели - 32 мартобря.");
+
+int N = int.Parse (Console.ReadLine());
+for ( int i = 1; i <= N; i++ )
+Console.WriteLine ( i*i*i);
