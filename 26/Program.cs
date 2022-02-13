5155ecedd5a8d5190922e6968260370d6d5fb25f
Alex_T <saler@inbox.ru>
2022-02-13 22:55:51 +0300

    Задания 26

---
+++ 26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+using static System. Console;
+
+Console.Write("Введите число A и нажмите клавишу Enter: ");
+double A = int.Parse(ReadLine());
+Console.Write("Введите степень числа А и нажмите клавишу Enter: ");
+double B = int.Parse(ReadLine());
+
+double s = 1;
+
+for (int i = 1; i <=B; i++)
+{
+    s = s * A;
+}
+
+WriteLine($"{s}");
