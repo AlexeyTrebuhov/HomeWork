7b875f397a34cfb7c51d8c1d5773d88c22e7c4ee
Alex_T <saler@inbox.ru>
2022-02-14 02:07:59 +0300

    Задания 28

---
+++ 28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Подсчитать сумму цифр в числе
+
+class Program
+   {
+      static void Main(string[] args)
+      {
+      Console.WriteLine("Введите целое число:");
+      var s = Console.ReadLine();
+      int i = 0; 
+      int count = s.Length; 
+      int sum = 0;
+      {
+           
+            while (i < count)
+            {
+                if (char.IsDigit(s[i]))
+                   sum += s[i++];
+            }
+            Console.WriteLine(sum);
+ 
+        }
+    }
+ }
+
