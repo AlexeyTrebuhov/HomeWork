ecd09263fde8da16f15981795743ff6b2ba406b8
Alex_T <saler@inbox.ru>
2022-02-14 23:36:29 +0300

    Задания 32

---
+++ 32/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+using static System.Console;
+
+int[,] table = new int [2,4];
+
+Console.WriteLine();
+for (int count = 0; count <2; count++)
+{
+    for (int count1 = 0; count1 <4; count1++)
+    {   
+    table [count,count1] = new Random().Next(0,2);
+    
+    Console.Write($"{table[count,count1]}");
+    }
+    Console.WriteLine();
+    }
+Console.WriteLine();
+
