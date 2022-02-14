fe664ac081e85b330c551b42145984cc2b135e26
Alex_T <saler@inbox.ru>
2022-02-14 22:39:49 +0300

    Задания 30

---
+++ 30/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число больше ноля и  нажмите клавишу Enter: ");
+double N = double.Parse (Console.ReadLine());
+double z = -1;
+
+for ( int i = 1; i <= N; i++ )
+z = i*i*i;
+
+if ( z % 2 == 0 )
+{
+Console.WriteLine (" Это число четное");
+Console.WriteLine (z);}
+
+else
+{
+Console.WriteLine (" Это число нечетное. Попробуйте еще раз");
+}
