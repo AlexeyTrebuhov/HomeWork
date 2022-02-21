230a909c3d930137d495de9b395a0e9df36f087a
Alex_T <saler@inbox.ru>
2022-02-21 20:38:41 +0300

    Задания 48

---
+++ 48/Program.cs
@@ -0,0 +1,32 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+
+using static System.Console;
+using System.Collections.Generic;
+
+Console.Write("Введите значение высоты массива и нажмите клавишу Enter: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите значение ширины массива и нажмите клавишу Enter: ");
+int y = int.Parse(Console.ReadLine());
+
+int[,] table = new int[x, y];
+
+Console.WriteLine();
+for (int count = 0; count < x; count++)
+{
+    for (int count1 = 0; count1 < y; count1++)
+    {
+        table[count, count1] = new Random().Next(99, 1000);
+
+        Console.Write($"{table[count, count1] + " "}");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+    
+
+
+
+
