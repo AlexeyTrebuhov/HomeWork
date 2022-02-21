7cd5645ab9a5b0ed02f1742d9290518c87962471
Alex_T <saler@inbox.ru>
2022-02-21 21:35:39 +0300

    Задания 50-1

---
+++ 50-1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Поиск автомобильного номера
+
+using System.IO;
+using System;
+using System.Linq;
+
+StreamReader sr = new StreamReader("input");
+int yes = 0;
+int i = 0;
+string chars = "0123456789ABCEHKMOPTXYАВСЕНКМОРТХУ";
+while (!sr.EndOfStream)
+{
+    string number = sr.ReadLine();
+    if ((chars.IndexOf(number[0]) > 9)
+    && (chars.IndexOf(number[1]) < 9)
+    && (chars.IndexOf(number[2]) < 9)
+    && (chars.IndexOf(number[2]) >= 0)
+    && (chars.IndexOf(number[3]) < 9)
+    && (chars.IndexOf(number[3]) >= 0)
+    && (chars.IndexOf(number[4]) > 9)
+    && (chars.IndexOf(number[4]) >= 0)
+    && (chars.IndexOf(number[5]) > 9)
+    ) yes++;
+    i++;
+}
+
+Console.WriteLine($"{yes}  {i - yes}");
+
