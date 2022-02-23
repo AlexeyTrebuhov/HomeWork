917cda98dd7a9e103f932c16663e942daafeccea
Alex_T <saler@inbox.ru>
2022-02-24 01:13:05 +0300

    Задания 46

---
+++ 46/Program.cs
@@ -0,0 +1,48 @@
+﻿// Написать программу масштабирования фигуры
+
+using System.Linq;
+using static System.Console;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inputArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько у фигуры координатных точек ");
+            int z = int.Parse(Console.ReadLine());
+            int[,] mas = new int[2,z];
+            Console.WriteLine();
+            Console.WriteLine("Введите координаты точек");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+                    Console.Write("mas[" + i + "," + j + "]: ");
+                    mas[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+                    Console.Write(" mas[" + i + "," + j + "]: " + mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
