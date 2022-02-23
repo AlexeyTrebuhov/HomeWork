1add8afd4131e63cd008a3e4b29ce9570eee3cf7
Alex_T <saler@inbox.ru>
2022-02-23 17:58:57 +0300

    Задания 100

---
+++ 100/Program.cs
@@ -0,0 +1,28 @@
+﻿// Проверка совпадения букв в двух массивах
+
+using System.IO;
+using System;
+using System.Linq;
+
+StreamReader sr = new StreamReader("input");
+string[] names = sr.ReadLine().Split(" ");
+int [] name1 = names[0].ToString().ToLower().Select(x => Convert.ToInt32((byte)x)).OrderBy(x => x).ToArray();
+int [] name2 = names[1].ToString().ToLower().Select(x => Convert.ToInt32((byte)x)).OrderBy(x => x).ToArray();
+Console.WriteLine(name1);
+Console.WriteLine(name2);
+if (name1.Count() != name2.Length)
+{
+    Console.WriteLine("No1");
+    return;
+}
+
+for (int i=0; i < name1.Count();  i++)
+{
+    if (name1[i] != name2[i])
+    {
+    Console.WriteLine("No2");
+    return;
+    }
+}
+
+ Console.WriteLine("Yes!");
