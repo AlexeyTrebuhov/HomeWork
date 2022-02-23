0c487f02c837b9085101d8b0b85d8c3b05223077
Alex_T <saler@inbox.ru>
2022-02-23 23:22:54 +0300

    Задания 42

--- 42/Program.cs
+++ 42/Program.cs
@@ -1,41 +1,31 @@
 ﻿///Определить сколько чисел больше 0 введено с клавиатуры
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp8
+using System.Threading;
+class Sample
 {
-    class Program
-    {
-        static void Main(string[] args)
+public static void Main()
+{
+
+string userString = ""; 
+int summ = 0;
+Console.WriteLine("Для расчета введите число, выхода - букву");
+
+        do
         {
-            int value;
-            int summ = 0;
-            int z = 2;
-
-            Console.WriteLine();
-            Console.WriteLine("0 - выход из программы");
-            Console.WriteLine();
             
-            do
-            {
-                Console.Write("Введите число   ");
-                value = Int32.Parse(Console.ReadLine());
-
-                if (value > 0)
+            userString = Console.ReadLine();
+            double arg = Convert.ToDouble(userString);
+                if (arg * arg >= 0) 
+                {
+                if (arg > 0)
                     summ = summ + 1;
-                if (value <= 0)
+                if (arg <= 0)
                     summ = summ;
-            }
 
-            while (value != 0);
-
-            Console.WriteLine();
-            Console.WriteLine($"Чисел больше ноля {summ}");
-            Console.ReadKey();
-        }
-    }
+                Console.WriteLine($"Чисел больше ноля введено {summ}");
+                }
+          }
+        while (userString != "asd");
+}
 }
