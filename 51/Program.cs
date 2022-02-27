9d5da9ad656d62633b0ab42564c8ce97aca02268
Alex_T <saler@inbox.ru>
2022-02-27 19:47:01 +0300

    Задания 51

--- 51/Program.cs
+++ 51/Program.cs
@@ -1,2 +1,22 @@
 ﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n.
 
+using System;
+using static System.Console;
+using System.Collections.Generic;
+
+int x = 7;
+int y = 11;
+
+int[,] array = new int[x, y];
+Random random = new Random();
+
+Console.WriteLine();
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        array[i, j] = i + j;
+        Console.Write($"{array[i, j] + " "}");
+    }
+    Console.WriteLine();
+}
