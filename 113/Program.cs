310ad21039b545c380d874c4eda51797a3559786
Alex_T <saler@inbox.ru>
2022-02-28 20:44:52 +0300

    Задания 113

---
+++ 113/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от M до N, N и M  заданы
+
+string NumberLook(int M, int N)
+{
+string result = string.Empty;
+if ( M==N) return N.ToString();
+else return M+" "+NumberLook (M+1,N);
+}
+Console.WriteLine(NumberLook(10,20));
