1e461f1fee5b9632b87860ec94b9b26e8d50b5ad
Alex_T <saler@inbox.ru>
2022-02-28 21:16:13 +0300

    Задания 115

---
+++ 115/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+using System;
+
+int method(int A, int B)
+{
+    if (B==0) return 1;
+    else return A + method (A,B-1);      ;
+}
+Console.WriteLine(method (2,7));
