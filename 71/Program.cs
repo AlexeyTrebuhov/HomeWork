a7176c1cedcd720e77db9f6a8bbc9a5a017f1bd7
Alex_T <saler@inbox.ru>
2022-03-03 21:09:13 +0300

    Задания 71

---
+++ 71/Program.cs
@@ -0,0 +1,16 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+//Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+
+using System;
+string charsWords = "аисв";
+int countCharsInWords = 4;
+PrintAllWords(charsWords, countCharsInWords, "");
+
+
+
+void PrintAllWords(string alphabet, int length, string prefix)
+{
+    if (length == 0) Console.WriteLine(prefix);
+    else
+        foreach (char c in alphabet) PrintAllWords(alphabet, length - 1, prefix + c);
+}
