9ff85c0ea96876bea48f4b521d2f00952c3df6d3
Alex_T <saler@inbox.ru>
2022-03-04 23:14:11 +0300

    Задания 110

---
+++ 110/Program.cs
@@ -0,0 +1,22 @@
+﻿///Элементы массива уменьшить на 20, умножить на последний элемент и 
+///увеличить на число В
+
+Console.Write("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[3];
+Random r = new Random();
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = r.Next(-10, 10);
+    Console.WriteLine(a[i]);
+}
+int last = a[a.Length - 1];
+Console.WriteLine("Новый массив");
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] -= 20;
+    a[i] *= last;
+    a[i] += b;
+    Console.WriteLine(a[i]);
+}
+Console.ReadKey();
