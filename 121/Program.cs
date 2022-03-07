91210b950bfdffd5c8b21d4626d54907ec6ee175
Alex_T <saler@inbox.ru>
2022-03-07 23:23:13 +0300

    Задания 121

--- 121/Program.cs
+++ 121/Program.cs
@@ -1,21 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Конвертация из десятичного в двоичное
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-public class Program
-{
-    private static void Main(string[] args)
-    {
 
-        int n = 10;
-        for (int i = 0; i < n; i++)
-        {
-
-            Console.WriteLine("{0} десятичное = {1} ", Convert.ToString(i, 2).PadLeft(8, '0'), i);
-        }
-        Console.ReadKey();
-
-    }
-}
+string bin = "101011";
+int dec = Convert.ToInt32(bin, 2);
+Console.WriteLine (dec);
