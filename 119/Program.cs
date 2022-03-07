1df88cc4eb4b2db981131f071297828d4def43e1
Alex_T <saler@inbox.ru>
2022-03-07 22:41:37 +0300

    Задания 119

---
+++ 119/Program.cs
@@ -0,0 +1,26 @@
+﻿// 
+System.Console;
+
+static int Ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ack(m - 1, 1);
+    }
+    else
+    {
+        return Ack(m - 1, Ack(m, n - 1));
+    }
+
+}
+private void button13_Click(object sender, EventArgs e)
+{
+    int a = int.Parse(textBox24.Text);
+    int b = int.Parse(textBox25.Text);
+    string eee = Ack(a, b).ToString();
+    label50.Text = eee;
+}
