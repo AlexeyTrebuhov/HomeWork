67688ba30f91d344b4c0f015f54da88b50e7e81a
Alex_T <saler@inbox.ru>
2022-03-07 18:23:35 +0300

    Задания 116

--- 116/Program.cs
+++ 116/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Два друга идут навстречу, а собака бегает между ними
 
-int rasst = 2000;
-int V1 = 5;
+int rasst = 4000;
+int V1 = 10;
 int V2 = 5;
 int Vsob = 15;
 double count = 0;
@@ -15,5 +15,10 @@ clock = rasst / (V2 + Vsob);
 rasstost = rasst - clock*(V2 + V1); 
 rasst = rasstost;
 count = count + 1;
+clock = rasst / (V1 + Vsob);
+rasstost = rasst - clock * (V2 + V1);
+rasst = rasstost;
+count = count + 1;
+
 }
 Console.WriteLine($"Собака пробежит между друзьями {count} раз");
