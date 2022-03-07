5391482ce84f8f43c34938f4965c197b9020ee72
Alex_T <saler@inbox.ru>
2022-03-07 23:03:39 +0300

    Задания 121

--- 121/Program.cs
+++ 121/Program.cs
@@ -6,14 +6,16 @@ using System.Linq;
 using System.Text;
 public class Program
 {
-   public static void Main(string[] args)
-{
-    int i = 4; // это задаем степень числа 2 ( прописываем двузначное число)
-   
-        string sBynary = string.Join("", Enumerable.Range(0, 9) // 0-9 - Это длина числа в двоичной системе
-            .Select((v, p) => p + 1 == i || p + 1 == 1 - i - 1 ? '1' : '0'));
-        Console.WriteLine("{0} десятичное = {1} ", sBynary, Convert.ToInt32(sBynary, 2));
-   
-    Console.ReadKey();
-}
+    private static void Main(string[] args)
+    {
+
+        int n = 10;
+        for (int i = 0; i < n; i++)
+        {
+
+            Console.WriteLine("{0} десятичное = {1} ", Convert.ToString(i, 2).PadLeft(8, '0'), i);
+        }
+        Console.ReadKey();
+
+    }
 }
