ace78d8a0e0109b72fbce8a59c34a0615c456b70
Alex_T <saler@inbox.ru>
2022-03-08 11:12:39 +0300

    Задача 123

--- 123/Program.cs
+++ 123/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Переворот строки
 
-Console.WriteLine(new string("Hello world!".ToCharArray().Reverse().ToArray()));
+///Console.WriteLine(new string("Hello world!".ToCharArray().Reverse().ToArray()));
+/// второй вариант ниже
+
+
+
+    Console.WriteLine("Введите значение");
+    string s = Console.ReadLine();
+    char[] str = s.ToCharArray();
+    Array.Reverse(str);
+    Console.Write(str);
+    Console.ReadKey();
