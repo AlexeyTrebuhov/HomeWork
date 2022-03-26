00b309074212822448236caecbb4d7575ca581c5
Alex_T <saler@inbox.ru>
2022-03-26 15:52:04 +0300

    Задача 1242

---
+++ 124/Program.cs
@@ -0,0 +1,8 @@
+﻿// Создание таблиц на 10.000 строк
+
+string s="";
+for (int i=0; i<10001; i++)
+{
+    s+=$"{i}; фио_{i}; Мама={new Random().Next(10)}; Папа={new Random().Next (11,22)}";
+}
+File.WriteAllText("db.csv",s);
