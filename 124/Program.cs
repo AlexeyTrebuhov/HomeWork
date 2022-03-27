938554cf278e97e2294209373eaad6cd371c4989
Alex_T <saler@inbox.ru>
2022-03-27 21:30:29 +0300

    Задача 124

--- 124/Program.cs
+++ 124/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Создание таблиц на 10.000 строк
 
 string s="";
-for (int i=0; i<10001; i++)
+for (int i=0; i<10; i++)
 {
-    s+=$"{i}; фио_{i}; Мама={new Random().Next(10)}; Папа={new Random().Next (11,22)}";
+    s+=$"{i}; фио_{i}; Мама={new Random().Next(10)}; Папа={new Random().Next (11,22)};\n";
 }
 File.WriteAllText("db.csv",s);
