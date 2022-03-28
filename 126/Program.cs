b02f7d550459d07e9fe6ea08815afd546dffa3d4
Alex_T <saler@inbox.ru>
2022-03-28 22:56:44 +0300

    Задача 126

---
+++ 126/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задание со звездочкой (домашнее)
+// SELECT ФИО, Тел, Коммент FROM Люди LEFT JOIN Телефоны ON id = Чей телефон
+
+
+select
+
+pers_full_name,
+tel_number,
+tel_comm
+
+from pers left join tel on pers.pers_id = tel.tel_id
+
+// Выборка проведена в PostegreSQL, итог выборки - файл "Выборка"
