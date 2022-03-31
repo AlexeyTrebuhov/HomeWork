030f7df402b11d21bd6681d23e5ab29a56d60859
Alex_T <saler@inbox.ru>
2022-03-31 22:00:29 +0300

    Задача 127

---
+++ 127/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+using static System.Console;
+
+int[] table = new int[999];
+Console.WriteLine();
+Console.WriteLine("Это исходный массив чисел");
+for (int count = 0; count < 999; count++)
+{
+        table[count] = new Random().Next(0,9999);
+
+        Console.Write($"{table[count]} ");
+        //Console.WriteLine();
+} 
+    
+Console.WriteLine();
+//ReadLine();
