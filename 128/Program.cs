79d878d5d1af7aa168ede440fc06110cc545e316
Alex_T <saler@inbox.ru>
2022-04-02 01:11:23 +0300

    Задача 128

--- 128/Program.cs
+++ 128/Program.cs
@@ -7,12 +7,23 @@ class Test
     public static void Main()
     {
         string path = @"D:\Temp\text.txt";
-
+       
+        // Open the file to read from.
         string[] readText = File.ReadAllLines(path, Encoding.UTF8);
         foreach (string s in readText)
         {
             Console.WriteLine(s);
-        }
+                 
+ int [] newarray = new int [5];
+
+       for ( int i = 0; i < s.Length; i ++)
+          {
+         if (s.Length <= 3)
+        newarray[i] = s[i];
+         }
+        Console.WriteLine (s);
+       }
+    }
 
     }
-}
+
