990dbbce3a333548cc1545d56cb4c16a01d200cc
Alex_T <saler@inbox.ru>
2022-04-02 16:21:15 +0300

    Задача 130

---
+++ 130/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача контрольная
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+ 
+namespace wordscount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            string[] textMass;
+            StreamReader sr = new StreamReader(@"D:\Temp\text.txt");
+
+            while (sr.EndOfStream != true)
+            {
+                s += sr.ReadLine();
+            }
+            textMass = s.Split(' ');
+        
+for ( int i = 0; i < textMass.Length; i++)
+{
+    Console.WriteLine (textMass[i]);
+}
+            Console.WriteLine(textMass.Length);
+
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
