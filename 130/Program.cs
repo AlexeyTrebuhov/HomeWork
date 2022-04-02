55f16baa8369349f5df8bc392af93ec22ddd170c
Alex_T <saler@inbox.ru>
2022-04-02 17:38:09 +0300

    Задача 130

--- 130/Program.cs
+++ 130/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
- 
+
 namespace wordscount
 {
     class Program
@@ -24,20 +24,20 @@ namespace wordscount
             textMass = s.Split(' ');
             Console.WriteLine();
 
-           Console.WriteLine("Это исходный массив строк, полученный из текстового файла");
+            Console.WriteLine("Это исходный массив строк, полученный из текстового файла");
             Console.WriteLine();
 
- int count = 0;
+            int count = 0;
 
-for ( int i = 0; i < textMass.Length; i++)
-{
-    if ( textMass[i].Length < 4)
-    {
-        count = count + 1;
-    }
-    Console.WriteLine (textMass[i]);
-    
-}
+            for (int i = 0; i < textMass.Length; i++)
+            {
+                if (textMass[i].Length < 4)
+                {
+                    count = count + 1;
+                }
+                Console.WriteLine(textMass[i]);
+
+            }
             Console.WriteLine();
             Console.WriteLine($"Количество строк с тремя символами и меньше = {count}");
             Console.WriteLine();
@@ -46,20 +46,20 @@ for ( int i = 0; i < textMass.Length; i++)
 
             string mini = "";
 
-for (int i = 0; i < textMass.Length; i++)
+            for (int i = 0; i < textMass.Length; i++)
             {
                 if (textMass[i].Length < 4)
                 {
-                    mini = String.Concat (mini, " , ");
+                    mini = String.Concat(mini, " , ");
 
                     mini = String.Concat(mini, textMass[i]);
                 }
-              
+
 
             }
 
             Console.WriteLine(mini);
             Console.WriteLine();
-  }
+        }
     }
 }
